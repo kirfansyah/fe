28daa1fa183a79fbaa876ad45851216a984d04b2
Rahul Mukhti <cengrahul00@gmail.com>
2025-10-06 15:50:40 +0700

    Modif Report Page

--- pages/admin/report.jsx
+++ pages/admin/report.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Search, Download, SlidersHorizontal } from "lucide-react";
