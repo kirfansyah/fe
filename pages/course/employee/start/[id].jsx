81d54e7952161d5c742dd1b97990aff5148ac5fe
Ozzy Qani <ozzy.qani@yahoo.com>
2025-10-23 09:22:02 +0700

    Update [id].jsx

--- pages/course/employee/start/[id].jsx
+++ pages/course/employee/start/[id].jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import CoursePlayer from "@/components/course/CoursePlayer";
+import CoursePlayer from "@/components/Course/CoursePlayer";
 import CourseLayout from "@/layouts/CourseLayout";
 
 export default function StartCoursePage() {
