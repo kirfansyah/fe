5286c7a87bc8c08b0541e2355b19f88c5a14e7de
Rahul Mukhti <cengrahul00@gmail.com>
2025-11-28 09:17:09 +0700

    add report modul

--- components/Sidebars/Sidebar.js
+++ components/Sidebars/Sidebar.js
@@ -32,7 +32,7 @@ export default function Sidebar() {
       label: listLanguage.course_management,
       link: "/course/management",
     },
-    { icon: FileText, label: listLanguage.report, link: "/course/1" },
+    { icon: FileText, label: listLanguage.report, link: "/admin/report" },
     { icon: MessageSquare, label: listLanguage.feedback, link: "/course/2" },
     { icon: Library, label: listLanguage.library, link: "/library/ebook" },
     {
