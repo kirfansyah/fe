ba426ec9ce3108c3a924b63807f5806af4faf117
Kiki Irfansyah <42080903+kirfansyah@users.noreply.github.com>
2025-12-09 08:55:10 +0700

    api/services

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //   baseURL: "/api/v1/",
-  baseURL: "/api/proxy",
+  baseURL: "/api/v1/",
+  //   baseURL: "/api/proxy",
   headers: {
     "Content-Type": "application/json",
   },
